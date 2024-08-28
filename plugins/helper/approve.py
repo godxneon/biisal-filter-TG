81fcb7ddcf877cf6a17dfe47bf1d79e6a9fa5051
HacKer Jr ~ TG <83758692+godxneon@users.noreply.github.com>
2024-08-28 21:49:01 +0530

    🥶

---
+++ plugins/helper/approve.py
@@ -0,0 +1,29 @@
+import os
+import asyncio
+import random
+import pytz
+import datetime
+from pyrogram import Client, filters
+from pyrogram.types import Message, User, InlineKeyboardMarkup, InlineKeyboardButton, ChatJoinRequest
+from info import CHAT_ID, TEXT, APPROVED, APICS
+
+@Client.on_chat_join_request((filters.group | filters.channel) & filters.chat(CHAT_ID) if CHAT_ID else (filters.group | filters.channel))
+async def autoapprove(client, message: ChatJoinRequest):
+    chat=message.chat 
+    user=message.from_user 
+    print(f"{user.first_name} Joined (Approved)") 
+    await client.approve_chat_join_request(chat_id=chat.id, user_id=user.id)       
+    buttons = [[
+                InlineKeyboardButton('🎭 GROUP', url='https://t.me/KLMovieGroup'),       
+                InlineKeyboardButton('CHANNEL 🎫', url='https://t.me/team_KL')
+             ],[
+                InlineKeyboardButton('♻️ Movie Request Group ♻️', url='https://t.me/KeralaRockers_Group')
+              ]]      
+    await client.send_photo(
+        photo=random.choice(APICS),
+        chat_id=message.from_user.id, 
+        caption=TEXT.format(mention=user.mention, title=chat.title),
+        reply_markup=InlineKeyboardMarkup(buttons)
+        )   
+#    await asyncio.sleep(35)
+#    await k.delete()
