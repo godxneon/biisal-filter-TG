9dc8f270e2f2200ff5a9d82ec7dd5b57cc7be00b
HacKer Jr ~ TG <83758692+godxneon@users.noreply.github.com>
2024-10-06 22:31:16 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -197,13 +197,7 @@ async def next_page(bot, query):
             ],
         )
     if settings["link"]:
-        links = ""
-        for file_num, file in enumerate(files, start=offset+1):
-            links += f"""<b>\n\n♻️ <a href=https://t.me/{temp.U_NAME}?start=file_{query.message.chat.id}_{file.file_id}>[{get_size(file.file_size)}] {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@') and not x.startswith('www.'), file.file_name.split()))} ({file_num})</a></b>"""
-    else:
-        btn = [[InlineKeyboardButton(text=f"📁 {get_size(file.file_size)} ⊳ {formate_file_name(file.file_name)}", url=f'https://telegram.dog/{temp.U_NAME}?start=file_{query.message.chat.id}_{file.file_id}'),]
-                for file in files
-	      ]	
+        links = ""        
         await query.message.edit_text(cap + links + del_msg, disable_web_page_preview=True, parse_mode=enums.ParseMode.HTML, reply_markup=InlineKeyboardMarkup(btn))
         return        
     try:
