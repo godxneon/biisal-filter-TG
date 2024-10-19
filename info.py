9926ecf8bd05f5a48ca81e7f76ed725b1f24c18e
HacKer Jr ~ TG <83758692+godxneon@users.noreply.github.com>
2024-10-19 16:51:38 +0530

    Update info.py

--- info.py
+++ info.py
@@ -61,7 +61,7 @@ SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', '-1001728602477'))
 request_channel = environ.get('REQUEST_CHANNEL', '-1002079370592')
 REQUEST_CHANNEL = int(request_channel) if request_channel and id_pattern.search(request_channel) else None
 UPI_PAY_LOGS = int(environ.get('UPI_PAY_LOGS', '-1008000054678'))
-MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', '-1002000054678'))
+MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', '-1001845390899'))
 
 #Auto Approve 
 APICS = (environ.get('APICS', 'https://telegra.ph/file/251b7e8892f3d6fca2536.jpg https://telegra.ph/file/4fc1332714da789d5d960.jpg')).split()
