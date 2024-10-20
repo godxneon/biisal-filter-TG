6eb595114f3b8c16d7b2ea21f97319aa0b73b4b1
HacKer Jr ~ TG <83758692+godxneon@users.noreply.github.com>
2024-10-20 18:29:39 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -40,7 +40,7 @@ async def get_imdb(file_name , post_mode):
             title=imdb.get('title'),
             rating=imdb.get('rating'),
             genres=imdb.get('genres'),
-            runtime=imdb.get('runtimes'),
+            duration=imdb.get('runtime'),
             description=imdb.get('plot'),
             file_name=file_name
         )
