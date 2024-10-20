e6d4e913a4edf4c475ebcd6d3482cf873f54d937
HacKer Jr ~ TG <83758692+godxneon@users.noreply.github.com>
2024-10-21 01:58:45 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -41,13 +41,11 @@ async def get_imdb(file_name):
     imdb_file_name = await movie_name_format(file_name)
     imdb = await get_poster(imdb_file_name)
     if imdb:
-        caption = MOVIES_UPDATE_TXT.format(
-            title=imdb.get('title'),
+        caption = (title=imdb.get('title'),
             rating=imdb.get('rating'),
             genres=imdb.get('genres'),
             description=imdb.get('plot'),
             file_name=file_name,
-            movie_name=movie_name
         )
         return imdb.get('title'), imdb.get('poster'), caption
     return None, None, None 
@@ -90,7 +88,7 @@ async def send_movie_updates(bot, file_name, caption, file_id):
             return 
         processed_movies.add(movie_name)    
         poster_url = await get_imdb(movie_name)
-        #caption_message = f"#New_File_Added ✅\n\nFile_Name:- <code>{movie_name}</code>\n\nLanguage:- {language}\n\nQuality:- {quality}\n{rating}"    
+        caption_message = f"#New_File_Added ✅\n\nFile_Name:- <code>{movie_name}</code>\n\nLanguage:- {language}\n\nQuality:- {quality}\n{rating}"    
         movie_update_channel = await db.movies_update_channel_id()    
         btn = [
             [InlineKeyboardButton('Get File', url=f'https://t.me/{temp.U_NAME}?start=pm_mode_file_{ADMINS[0]}_{file_id}')]
@@ -98,11 +96,11 @@ async def send_movie_updates(bot, file_name, caption, file_id):
         reply_markup = InlineKeyboardMarkup(btn)
         if poster_url:
             await bot.send_photo(movie_update_channel if movie_update_channel else MOVIE_UPDATE_CHANNEL, 
-                                 photo=poster_url, caption=caption, reply_markup=reply_markup)
+                                 photo=poster_url, caption=caption_message, reply_markup=reply_markup)
         else:
             no_poster = "https://envs.sh/pTu.jpg"
             await bot.send_photo(movie_update_channel if movie_update_channel else MOVIE_UPDATE_CHANNEL, 
-                                 photo=no_poster, caption=caption, reply_markup=reply_markup)  
+                                 photo=no_poster, caption=caption_message, reply_markup=reply_markup)  
     except Exception as e:
         print('Failed to send movie update. Error - ', e)
         await bot.send_message(LOG_CHANNEL, f'Failed to send movie update. Error - {e}')
