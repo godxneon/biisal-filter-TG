0dcba8499d86e8a73dfdd9d4f7664e296535f91b
HacKer Jr ~ TG <83758692+godxneon@users.noreply.github.com>
2024-10-21 18:06:05 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -51,21 +51,49 @@ async def get_imdb(file_name, caption):
         return imdb.get('title'), imdb.get('poster'), caption
     return None, None, None 
 
-async def send_movie_updates(bot, file_name, file_id):
-    imdb_title, poster_url, caption = await get_imdb(file_name)
-    #if not post_mode.get('singel_post_mode' , True):
-    if imdb_title in processed_movies:
-        return
-        processed_movies.add(imdb_title)
-    if not poster_url or not caption:
-        return
-    btn = [
-        [InlineKeyboardButton('Get File', url=f'https://t.me/{temp.U_NAME}?start=pm_mode_file_{ADMINS[0]}_{file_id}')]
-    ]
-    reply_markup = InlineKeyboardMarkup(btn)
-    movie_update_channel =await db.movies_update_channel_id()
+async def send_movie_updates(bot, file_name, caption, file_id):
     try:
-        await bot.send_photo(movie_update_channel if movie_update_channel else MOVIE_UPDATE_CHANNEL, photo=poster_url, caption=caption, reply_markup=reply_markup)
+        year_match = re.search(r"\b(19|20)\d{2}\b", caption)
+        year = year_match.group(0) if year_match else None      
+        pattern = r"(?i)(?:s|season)0*(\d{1,2})"
+        season = re.search(pattern, caption)
+        if not season:
+            season = re.search(pattern, file_name) 
+        if year:
+            file_name = file_name[:file_name.find(year) + 4]      
+        if not year:
+            if season:
+                season = season.group(1) if season else None       
+                file_name = file_name[:file_name.find(season) + 1]
+        qualities = ["ORG", "org", "hdcam", "HDCAM", "HQ", "hq", "HDRip", "hdrip", 
+                     "camrip", "WEB-DL" "CAMRip", "hdtc", "predvd", "DVDscr", "dvdscr", 
+                     "dvdrip", "dvdscr", "HDTC", "dvdscreen", "HDTS", "hdts"]
+        quality = await check_qualities(caption.lower(), qualities) or "HDRip"
+        language = ""
+        nb_languages = ["Hindi", "Bengali", "English", "Marathi", "Tamil", "Telugu", "Malayalam", "Kannada", "Punjabi", "Gujrati", "Korean", "Japanese", "Bhojpuri", "Dual", "Multi"]    
+        for lang in nb_languages:
+            if lang.lower() in caption.lower():
+                language += f"{lang}, "
+        language = language.strip(", ") or "Not Idea"
+        movie_name = await movie_name_format(file_name)    
+        if movie_name in processed_movies:
+            return 
+        processed_movies.add(movie_name)    
+        poster_url = await get_imdb(movie_name)
+        caption_message = f"#New_File_Added ✅\n\nFile_Name:- <code>{movie_name}</code>\n\nLanguage:- {language}\n\nQuality:- {quality}"    
+        movie_update_channel = await db.movies_update_channel_id()    
+        btn = [
+            [InlineKeyboardButton('Get File', url=f'https://t.me/{temp.U_NAME}?start=pm_mode_file_{ADMINS[0]}_{file_id}')]
+        ]
+        reply_markup = InlineKeyboardMarkup(btn)
+        if poster_url:
+            await bot.send_photo(movie_update_channel if movie_update_channel else MOVIE_UPDATE_CHANNEL, 
+                                 photo=poster_url, caption=caption_message, reply_markup=reply_markup)
+        else:
+            no_poster = "https://telegra.ph/file/88d845b4f8a024a71465d.jpg"
+            await bot.send_photo(movie_update_channel if movie_update_channel else MOVIE_UPDATE_CHANNEL, 
+                                 photo=no_poster, caption=caption_message, reply_markup=reply_markup)  
     except Exception as e:
-        print('Error in send_movie_updates', e)
-        pass
+        print('Failed to send movie update. Error - ', e)
+        await bot.send_message(LOG_CHANNEL, f'Failed to send movie update. Error - {e}')
+    
