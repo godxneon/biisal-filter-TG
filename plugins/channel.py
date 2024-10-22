53de5c5795129ffeb93f6c72fc705ba89aaa1066
HacKer Jr ~ TG <83758692+godxneon@users.noreply.github.com>
2024-10-22 21:30:21 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -45,7 +45,7 @@ def name_format(file_name: str):
 async def get_imdb(file_name):
     imdb_file_name = name_format(file_name)
     imdb = await get_poster(imdb_file_name)
-    file_name = f'File Name : <code>{formate_file_name(file_name)}</code>' if post_mode.get('singel_post_mode' , True) else ''
+    file_name = f'File Name : <code>{formate_file_name(file_name)}</code>' 
     if imdb:
         caption = script.MOVIES_UPDATE_TXT.format(
             title=imdb.get('title'),
