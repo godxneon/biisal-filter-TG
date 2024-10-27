649883231aad736e0457e81e009b0e3d7d51754d
HacKer Jr ~ TG <83758692+godxneon@users.noreply.github.com>
2024-10-28 02:58:12 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -83,7 +83,7 @@ async def send_movie_updates(bot, file_name, caption, file_id):
             return 
         processed_movies.add(movie_name)    
         poster_url = await get_imdb(movie_name)
-        caption_message = f"<b>```🧧 𝙉𝙚𝙬 𝙊𝙏𝙏 𝙍𝙚𝙡𝙚𝙖𝙨𝙚𝙨 🥳```\n📻 Title : {movie_name}\n🔊 Language : {language}\n💿 Quality : {quality}\n\n➠ Uploaded By : @Team_KL</b>"    
+        caption_message = f"<b>📻 Title : {movie_name}\n🔊 Language : {language}\n💿 Quality : {quality}\n\n➠ Uploaded By : @Team_KL</b>"    
         search_movie = movie_name.replace(" ", '-')
         movie_update_channel = await db.movies_update_channel_id()    
         btn = [[
