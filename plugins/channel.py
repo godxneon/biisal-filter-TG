776ea3d598fc05ab209a805bb093dad62bd7ec0f
HacKer Jr ~ TG <83758692+godxneon@users.noreply.github.com>
2024-11-18 22:37:10 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -82,12 +82,8 @@ async def send_movie_updates(bot, file_name, caption, file_id):
         if movie_name in processed_movies:
             return 
         processed_movies.add(movie_name)    
-        poster_url = await get_imdb(movie_name)
-        imdb = await get_poster(file_name)
-        if imdb:
-            rating=imdb.get('rating'),
-            genres=imdb.get('genres')            
-        caption_message = f"<b>📻 Title : {movie_name}\n🔊 Language : {language}\n🌟 Rating: {imdb['{genres}']}\n💿 Quality : {quality}\n\n➠ Uploaded By : @Team_KL</b>"    
+        poster_url = await get_imdb(movie_name)            
+        caption_message = f"<b>📻 Title : {movie_name}\n🔊 Language : {language}\n💿 Quality : {quality}\n\n➠ Uploaded By : @Team_KL</b>"    
         search_movie = movie_name.replace(" ", '-')
         movie_update_channel = await db.movies_update_channel_id()    
         btn = [[
