bd06814a69ea09083bdfbd2898ec9bce3b9eb306
HacKer Jr ~ TG <83758692+godxneon@users.noreply.github.com>
2024-12-29 13:27:00 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -81,9 +81,7 @@ async def send_movie_updates(bot, file_name, caption, file_id):
         search_movie = movie_name.replace(" ", '-')
         movie_update_channel = await db.movies_update_channel_id()    
         btn = [[            
-            InlineKeyboardButton('⚠️ Get Sample File 📂', url=f'https://t.me/{temp.U_NAME}?start=pm_mode_file_{ADMINS[0]}_{file_id}')
-        ],[
-            InlineKeyboardButton('🔮 Click Here To Search 🔍', url=f'https://t.me/{temp.U_NAME}?start=getfile-{search_movie}')
+            InlineKeyboardButton('Click Here To Download', url=f'https://t.me/{temp.U_NAME}?start=getfile-{search_movie}')
         ]]
         reply_markup = InlineKeyboardMarkup(btn)
         if poster_url:
