a4b0835923cb9c84efa93fdc3dde33359383fdbd
HacKer Jr ~ TG <83758692+godxneon@users.noreply.github.com>
2025-01-22 02:04:22 +0530

    Update gfilters.py

--- plugins/gfilters.py
+++ plugins/gfilters.py
@@ -120,14 +120,20 @@ async def deletegfilter(client, message):
 
     await delete_gfilter(message, query, 'gfilters')
 
+
 @Client.on_message(filters.command('delallg') & filters.user(ADMINS))
-async def delallgfilters(client, message):
+async def delallgfill(client, message):
     await message.reply_text(
             f"Do you want to continue??",
             reply_markup=InlineKeyboardMarkup([
-                [InlineKeyboardButton(text="YES",callback_data="gfiltersdeleteallconfirm")],
-                [InlineKeyboardButton(text="CANCEL",callback_data="gfiltersdeleteallcancel")]
+                [InlineKeyboardButton(text="YES",callback_data="gconforme")],
+                [InlineKeyboardButton(text="CANCEL",callback_data="close_data")]
             ]),
             quote=True
         )
 
+
+@Client.on_callback_query(filters.regex("gconforme"))
+async def dellacbd(client, message):
+    await del_allg(message.message, 'gfilters')
+    return await message.reply("👍 Done")
