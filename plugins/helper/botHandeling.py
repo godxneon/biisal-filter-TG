8a67fa2e025efba023948eb110fad16136794b5b
HacKer Jr ~ TG <83758692+godxneon@users.noreply.github.com>
2025-01-22 00:06:47 +0530

    Update botHandeling.py

--- plugins/helper/botHandeling.py
+++ plugins/helper/botHandeling.py
@@ -1,1 +1,33 @@
-hi
+from pyrogram import Client, filters , enums
+from pyrogram.types import InlineKeyboardMarkup , InlineKeyboardButton
+from info import ADMINS 
+import re
+from database.users_chats_db import db
+
+@Client.on_message(filters.command("set_muc") & filters.user(ADMINS))
+async def set_muc_id(client, message):
+    try:
+        id = message.command[1]
+        if id:
+            is_suc = await db.movies_update_channel_id(int(id))
+            if is_suc:
+                await message.reply("Successfully set movies update  channel id : " + id)
+            else:
+                await message.reply("Failed to set movies update channel id : " + id)
+        else:
+            await message.reply("Invalid channel id : " + id)
+    except Exception as e:
+        print('Err in set_muc_id', e)
+        await message.reply("Failed to set movies channel id!")
+
+@Client.on_message(filters.command("del_muc") & filters.user(ADMINS))
+async def del_muc_id(client, message):
+    try:
+        is_suc = await db.del_movies_channel_id()
+        if is_suc:
+            await message.reply("Successfully deleted movies channel id")
+        else:
+            await message.reply("Failed to delete movies channel id")
+    except Exception as e:
+        print('Err in del_muc_id', e)
+        await message.reply("Failed to delete movies channel id!")
