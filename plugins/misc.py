1e58f7cea86ed32510ec9f6088f10db4f146d679
HacKer Jr ~ TG <83758692+godxneon@users.noreply.github.com>
2025-02-02 15:51:47 +0530

    Update and rename id.py to misc.py

---
+++ plugins/misc.py
@@ -0,0 +1,66 @@
+import os
+from info import ADMINS
+from speedtest import Speedtest, ConfigRetrievalError
+from pyrogram import Client, filters, enums
+from utils import get_size
+from datetime import datetime
+from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant, MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.ERROR)
+
+@Client.on_message(filters.command('id'))
+async def show_id(client, message):
+    chat_type = message.chat.type
+    if chat_type == enums.ChatType.PRIVATE:
+        await message.reply_text(f"<b>» ᴜꜱᴇʀ ɪᴅ - <code>{message.from_user.id}</code></b>")
+
+    elif chat_type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
+        await message.reply_text(f"<b>» ɢʀᴏᴜᴘ ɪᴅ - <code>{message.chat.id}</code></b>")
+
+    elif chat_type == enums.ChatType.CHANNEL:
+        await message.reply_text(f"<b>» ᴄʜᴀɴɴᴇʟ ɪᴅ - <code>{message.chat.id}</code></b>")
+
+@Client.on_message(filters.command('speedtest') & filters.user(ADMINS))
+async def speedtest(client, message):
+    #from - https://github.com/weebzone/WZML-X/blob/master/bot/modules/speedtest.py
+    msg = await message.reply_text("Initiating Speedtest...")
+    try:
+        speed = Speedtest()
+    except ConfigRetrievalError:
+        await msg.edit("Can't connect to Server at the Moment, Try Again Later !")
+        return
+    speed.get_best_server()
+    speed.download()
+    speed.upload()
+    speed.results.share()
+    result = speed.results.dict()
+    photo = result['share']
+    text = f'''
+➲ <b>SPEEDTEST INFO</b>
+┠ <b>Upload:</b> <code>{get_size(result['upload'])}/s</code>
+┠ <b>Download:</b>  <code>{get_size(result['download'])}/s</code>
+┠ <b>Ping:</b> <code>{result['ping']} ms</code>
+┠ <b>Time:</b> <code>{datetime.strptime(result['timestamp'], "%Y-%m-%dT%H:%M:%S.%fZ").strftime("%Y-%m-%d %H:%M:%S")}</code>
+┠ <b>Data Sent:</b> <code>{get_size(int(result['bytes_sent']))}</code>
+┖ <b>Data Received:</b> <code>{get_size(int(result['bytes_received']))}</code>
+
+➲ <b>SPEEDTEST SERVER</b>
+┠ <b>Name:</b> <code>{result['server']['name']}</code>
+┠ <b>Country:</b> <code>{result['server']['country']}, {result['server']['cc']}</code>
+┠ <b>Sponsor:</b> <code>{result['server']['sponsor']}</code>
+┠ <b>Latency:</b> <code>{result['server']['latency']}</code>
+┠ <b>Latitude:</b> <code>{result['server']['lat']}</code>
+┖ <b>Longitude:</b> <code>{result['server']['lon']}</code>
+
+➲ <b>CLIENT DETAILS</b>
+┠ <b>IP Address:</b> <code>{result['client']['ip']}</code>
+┠ <b>Latitude:</b> <code>{result['client']['lat']}</code>
+┠ <b>Longitude:</b> <code>{result['client']['lon']}</code>
+┠ <b>Country:</b> <code>{result['client']['country']}</code>
+┠ <b>ISP:</b> <code>{result['client']['isp']}</code>
+┖ <b>ISP Rating:</b> <code>{result['client']['isprating']}</code>
+'''
+    await message.reply_photo(photo=photo, caption=text)
+    await msg.delete()
