1b1fdc0fda8060a37cbcf9e70ca678220bdb146e
HacKer Jr ~ TG <83758692+godxneon@users.noreply.github.com>
2025-02-10 15:45:51 +0530

    Update and rename botHandeling.py to bothelpfull.py

--- plugins/helper/bothelpfull.py
+++ plugins/helper/bothelpfull.py
@@ -1,3 +1,4 @@
+import random
 from pyrogram import Client, filters , enums
 from pyrogram.types import InlineKeyboardMarkup , InlineKeyboardButton
 from info import ADMINS 
@@ -32,5 +33,14 @@ async def del_muc_id(client, message):
         print('Err in del_muc_id', e)
         await message.reply("Failed to delete movies channel id!")
 
-
-
+@Client.on_message(filters.regex("http") & filters.regex("www") | filters.regex("https") | filters.regex("t.me") & filters.incoming)
+async def nolink(bot, message):
+    user_id = message.from_user.id
+    if user_id in ADMINS: return 
+    await message.delete()
+	
+#@Client.on_message(filters.forwarded & filters.incoming)
+#async def forward(bot, message):
+    #user_id = message.from_user.id
+    #if user_id in ADMINS: return 
+    #await message.delete()
